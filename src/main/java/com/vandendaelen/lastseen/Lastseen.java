880ec18bd12568543b99f91d4b02be8cd60a7471
LotuxPunk <vandendaelenclement@gmail.com>
2020-09-21 18:46:48 +0200

    Initial commit

---
+++ src/main/java/com/vandendaelen/lastseen/Lastseen.java
@@ -0,0 +1,44 @@
+package com.vandendaelen.lastseen;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.vandendaelen.lastseen.business.PlayerDisconnectionHandler;
+import me.shedaniel.cloth.api.common.events.v1.PlayerLeaveCallback;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.text.LiteralText;
+
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+
+public class Lastseen implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        PlayerLeaveCallback.EVENT.register(
+                serverPlayerEntity -> PlayerDisconnectionHandler.getInstance().addPlayerDisconnection(serverPlayerEntity, LocalDateTime.now())
+        );
+
+        CommandRegistrationCallback.EVENT.register(
+                (dispatcher, dedicated) -> {
+                    dispatcher.register(
+                            CommandManager.literal("lastseen")
+                                    .then(CommandManager.argument("username", StringArgumentType.string())
+                                            .executes(context -> {
+                                                final PlayerDisconnectionHandler handler = PlayerDisconnectionHandler.getInstance();
+                                                String username = context.getArgument("username", String.class);
+                                                String result = handler.getPlayerLastTime(context.getSource().getMinecraftServer(), username);
+                                                if (result != null){
+                                                    context.getSource().sendFeedback(new LiteralText(username + " : " + result), true);
+                                                }
+                                                else {
+                                                    context.getSource().sendError(new LiteralText("Error : No player found"));
+                                                }
+
+                                                return Command.SINGLE_SUCCESS;
+                                            })
+                                    ));
+                }
+        );
+    }
+}
---
+++ src/main/java/com/vandendaelen/lastseen/business/PlayerDisconnectionHandler.java
@@ -0,0 +1,111 @@
+package com.vandendaelen.lastseen.business;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class PlayerDisconnectionHandler {
+    private static volatile PlayerDisconnectionHandler INSTANCE;
+    private HashMap<String, LocalDateTime> players;
+
+    public PlayerDisconnectionHandler() {
+        this.players = new HashMap<>();
+        readJson();
+    }
+
+    public static PlayerDisconnectionHandler getInstance(){
+        final PlayerDisconnectionHandler result = INSTANCE;
+        if (result != null){
+            return result;
+        }
+        synchronized (PlayerDisconnectionHandler.class){
+            if (INSTANCE == null){
+                INSTANCE = new PlayerDisconnectionHandler();
+            }
+            return INSTANCE;
+        }
+    }
+
+    public void addPlayerDisconnection(ServerPlayerEntity playerEntity, LocalDateTime date){
+        if (players.containsKey(playerEntity.getName().asString())){
+            players.replace(playerEntity.getName().asString(), date);
+        }
+        else {
+            players.put(playerEntity.getName().asString(), date);
+        }
+
+        final Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
+        final String prettyJson = prettyGson.toJson(players);
+
+        writeJson(prettyJson);
+    }
+
+    public String getPlayerLastTime(MinecraftServer server, String username){
+        if (Arrays.asList(server.getPlayerNames()).contains(username)){
+            return "now";
+        }
+        else {
+            if (players.containsKey(username)){
+                final LocalDateTime today = LocalDateTime.now();
+                final LocalDateTime lastDisconnection = players.get(username);
+                final long daysBetween = ChronoUnit.DAYS.between(lastDisconnection, today);
+                final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                final String formatDateTime = lastDisconnection.format(formatter);
+
+                return MessageFormat.format("{0} - ({1} day(s))", formatDateTime, daysBetween);
+            }
+            return  null;
+        }
+    }
+
+    private void writeJson(String json) {
+        FileWriter file = null;
+        try {
+            file = new FileWriter("lastseen.json");
+            file.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                file.flush();
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void readJson() {
+        final Gson gson = new Gson();
+        try {
+            Type type = new TypeToken<HashMap<String, LocalDateTime>>(){}.getType();
+            Reader reader = Files.newBufferedReader(Paths.get("lastseen.json"));
+            players = gson.fromJson(reader, type);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getUsernameList() {
+        return new ArrayList<>(players.keySet());
+    }
+}
